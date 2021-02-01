3ec5eeedc9074be0cf4541ac29cf95dc0c322746
lissms <felissms@gmail.com>
2021-02-01 17:38:51 +0100

    add splice methods and nehative index

--- array.js
+++ array.js
@@ -1,6 +1,26 @@
 let arr = ["felix", "lissy", "thomas", "pen", "love", "family", "day", "live"];
 
+/* 1- Splice */
+
 /* splice: from index X (the first paramenter) remove X element(the second parameter) */
 
 arr.splice(3, 2);
+
+/*  splice: replace elements with (the 3th paramenter and other)  */
+
+arr.splice(3, 5, "we", "are", "the", "family");
+
+/*  insert the elements without any removals. The splice method is also able to insert the elements without any removals */
+
+arr.splice(3, 0, "we", "are", "the", "family");
 console.log("arr", arr);
+
+/*  Here and in other array methods, negative indexes are allowed. They specify the position from the end of the array, like here: */
+let array = [1, 2, 5];
+
+// from index -1 (one step from the end)
+// delete 0 elements,
+// then insert 3 and 4
+
+array.splice(-1, 0, 3, 4);
+console.log("array", array);
