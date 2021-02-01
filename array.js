28c082ac0790027ccdeb96c232e1b02a330f4acb
lissms <felissms@gmail.com>
2021-02-01 11:16:33 +0100

    add splice methods

---
+++ array.js
@@ -0,0 +1,6 @@
+let arr = ["felix", "lissy", "thomas", "pen", "love", "family", "day", "live"];
+
+/* splice: from index X (the first paramenter) remove X element(the second parameter) */
+
+arr.splice(3, 2);
+console.log("arr", arr);
